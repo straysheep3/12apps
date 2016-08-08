2883a0a9efb84ccbe7bc0a1598f4ca62814a8d35
straysheep3 <getloose.kage@gmail.com>
2016-08-09 02:29:10 +0900

    userとlinkを関連付け

---
+++ reddit_12_1/db/migrate/20160808171030_add_user_id_to_links.rb
@@ -0,0 +1,7 @@
+# Linkにuser_idを加えるマイグレーション
+class AddUserIdToLinks < ActiveRecord::Migration[5.0]
+  def change
+    add_column :links, :user_id, :integer
+    add_index :links, :user_id
+  end
+end
