44c31b9b919be3142f60c679a615f36a14046302
straysheep3 <getloose.kage@gmail.com>
2016-08-10 17:08:48 +0900

    commentを投稿できるように作成

--- reddit_12_1/config/routes.rb
+++ reddit_12_1/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   root 'links#index'
   resources :links do
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
       put 'like', to: 'links#upvote'
       put 'dislike', to: 'links#downvote'
     end
+    resources :comments
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
